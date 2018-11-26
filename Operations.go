d52b6f7d822c048543abe416b363fca11dc44c6c
tbuchaillot <tomas.buchaillot@mercadolibre.com>
2018-11-26 10:18:06 -0300

    adding GT and LT

--- Operations.go
+++ Operations.go
@@ -2,15 +2,23 @@ package TFilter
 
 import (
 	"reflect"
+	"time"
 )
 
 const (
 	EQ = "EQ"
 	LT = "LT"
+	GT = "GT"
 )
 
 func (tf *TFilter) EQ(key string, value interface{}) *TFilter {
 	return tf.searcher(EQ, key, value)
+}
+func (tf *TFilter) LT(key string, value interface{}) *TFilter {
+	return tf.searcher(LT, key, value)
+}
+func (tf *TFilter) GT(key string, value interface{}) *TFilter {
+	return tf.searcher(GT, key, value)
 }
 
 func (tf *TFilter) searcher(operation string, key string, value interface{}) *TFilter {
@@ -24,6 +32,8 @@ func (tf *TFilter) searcher(operation string, key string, value interface{}) *TF
 			go searchEQ(tf.objs[i:i+maxChan], key, value, resChan)
 		case LT:
 			go searchLT(tf.objs[i:i+maxChan], key, value, resChan)
+		case GT:
+			go searchGT(tf.objs[i:i+maxChan], key, value, resChan)
 		}
 	}
 
@@ -74,8 +84,88 @@ func searchLT(objs []interface{}, key string, value interface{}, resChan chan []
 			zero := reflect.Zero(fieldRValue.Type()).Interface()
 			isZero := reflect.DeepEqual(fieldRValue.Interface(), zero)
 			if !isZero && fieldRType.Tag.Get("key") == key {
-				if fieldRValue.Interface() == value {
+				shouldAppend := false
+				switch fieldRValue.Interface().(type) {
+				case string:
+					objVal := fieldRValue.Interface().(string)
+					val := value.(string)
+					if len(objVal) > len(val) {
+						shouldAppend = true
+					}
+				case float32, float64:
+					objVal := fieldRValue.Interface().(float64)
+					val := value.(float64)
+					if objVal > val {
+						shouldAppend = true
+					}
+				case int, int16, int32, int64:
+					objVal := fieldRValue.Interface().(int64)
+					val := value.(int64)
+					if objVal > val {
+						shouldAppend = true
+					}
+				case time.Time:
+					objVal := fieldRValue.Interface().(time.Time)
+					val := value.(time.Time)
+					if val.Before(objVal) {
+						shouldAppend = true
+					}
+
+				}
+				if shouldAppend {
 					auxObj = append(auxObj, obj)
+
+				}
+			}
+		}
+	}
+	resChan <- auxObj
+}
+
+func searchGT(objs []interface{}, key string, value interface{}, resChan chan []interface{}) {
+	auxObj := []interface{}{}
+	for _, obj := range objs {
+		RValue := reflect.ValueOf(obj)
+		RType := reflect.TypeOf(obj)
+
+		for i := 0; i < RValue.NumField(); i++ {
+			fieldRType := RType.Field(i)
+			fieldRValue := RValue.Field(i)
+
+			zero := reflect.Zero(fieldRValue.Type()).Interface()
+			isZero := reflect.DeepEqual(fieldRValue.Interface(), zero)
+			if !isZero && fieldRType.Tag.Get("key") == key {
+				shouldAppend := false
+				switch fieldRValue.Interface().(type) {
+				case string:
+					objVal := fieldRValue.Interface().(string)
+					val := value.(string)
+					if len(objVal) < len(val) {
+						shouldAppend = true
+					}
+				case float32, float64:
+					objVal := fieldRValue.Interface().(float64)
+					val := value.(float64)
+					if objVal < val {
+						shouldAppend = true
+					}
+				case int, int16, int32, int64:
+					objVal := fieldRValue.Interface().(int64)
+					val := value.(int64)
+					if objVal < val {
+						shouldAppend = true
+					}
+				case time.Time:
+					objVal := fieldRValue.Interface().(time.Time)
+					val := value.(time.Time)
+					if val.After(objVal) {
+						shouldAppend = true
+					}
+
+				}
+				if shouldAppend {
+					auxObj = append(auxObj, obj)
+
 				}
 			}
 		}
