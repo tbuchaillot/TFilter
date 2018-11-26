97428d7aae5fc0dffd5273802c5c8e9b828cad96
tbuchaillot <tomas.buchaillot@mercadolibre.com>
2018-11-26 09:59:48 -0300

    fix

--- Operations.go
+++ Operations.go
@@ -4,13 +4,27 @@ import (
 	"reflect"
 )
 
+const (
+	EQ = "EQ"
+	LT = "LT"
+)
+
 func (tf *TFilter) EQ(key string, value interface{}) *TFilter {
+	return tf.searcher(EQ, key, value)
+}
+
+func (tf *TFilter) searcher(operation string, key string, value interface{}) *TFilter {
 	auxObj := []interface{}{}
 	maxChan := tf.size / maxChannels(tf.size)
 
 	resChan := make(chan []interface{}, maxChan)
 	for i := 0; i < tf.size; i += maxChan {
-		go searchEQ(tf.objs[i:i+maxChan], key, value, resChan)
+		switch operation {
+		case EQ:
+			go searchEQ(tf.objs[i:i+maxChan], key, value, resChan)
+		case LT:
+			go searchLT(tf.objs[i:i+maxChan], key, value, resChan)
+		}
 	}
 
 	for i := 0; i < tf.size; i += maxChan {
@@ -19,7 +33,9 @@ func (tf *TFilter) EQ(key string, value interface{}) *TFilter {
 			auxObj = append(auxObj, objs...)
 		}
 	}
-	tf.objs = auxObj
+	tf.objs = tf.objs[0:len(auxObj)]
+	copy(auxObj, tf.objs)
+
 	return tf
 }
 
@@ -28,6 +44,29 @@ func searchEQ(objs []interface{}, key string, value interface{}, resChan chan []
 	for _, obj := range objs {
 		RValue := reflect.ValueOf(obj)
 		RType := reflect.TypeOf(obj)
+
+		for i := 0; i < RValue.NumField(); i++ {
+			fieldRType := RType.Field(i)
+			fieldRValue := RValue.Field(i)
+
+			zero := reflect.Zero(fieldRValue.Type()).Interface()
+			isZero := reflect.DeepEqual(fieldRValue.Interface(), zero)
+			if !isZero && fieldRType.Tag.Get("key") == key {
+				if fieldRValue.Interface() == value {
+					auxObj = append(auxObj, obj)
+				}
+			}
+		}
+	}
+	resChan <- auxObj
+}
+
+func searchLT(objs []interface{}, key string, value interface{}, resChan chan []interface{}) {
+	auxObj := []interface{}{}
+	for _, obj := range objs {
+		RValue := reflect.ValueOf(obj)
+		RType := reflect.TypeOf(obj)
+
 		for i := 0; i < RValue.NumField(); i++ {
 			fieldRType := RType.Field(i)
 			fieldRValue := RValue.Field(i)
